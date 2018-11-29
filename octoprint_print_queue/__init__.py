82842fa126302a8ac9be225e3e3118763c09311d
fieldOfView <aldo@fieldofview.com>
2018-11-29 00:20:39 -0800

    Fix codestyle: mark private members

--- octoprint_print_queue/__init__.py
+++ octoprint_print_queue/__init__.py
@@ -14,31 +14,31 @@ class PrintQueuePlugin(octoprint.plugin.TemplatePlugin,
     octoprint.plugin.BlueprintPlugin,
     octoprint.plugin.EventHandlerPlugin):
 
-    print_queue = []
-    print_completed = False
-    uploads_dir = settings().getBaseFolder("uploads")
+    _print_queue = []
+    _print_completed = False
+    _uploads_dir = settings().getBaseFolder("uploads")
 
 
     # BluePrintPlugin (api requests)
     @octoprint.plugin.BlueprintPlugin.route("/queue", methods=["GET"])
     def get_queue(self):
         self._logger.info("PQ: getting queue")
-        return flask.jsonify(print_queue=self.print_queue)
+        return flask.jsonify(_print_queue=self._print_queue)
 
     @octoprint.plugin.BlueprintPlugin.route("/queue", methods=["POST"])
     @restricted_access
     def set_queue(self):
         # TODO: ensure the currently printing file remains on top of the list
         self._logger.info("PQ: received print queue from frontend")
-        last_print_queue = self.print_queue[:]
-        self.print_queue = []
+        last_print_queue = self._print_queue[:]
+        self._print_queue = []
         for v in flask.request.form:
             j = json.loads(v)
             for p in j:
-                self.print_queue += [p]
+                self._print_queue += [p]
 
-        if self.print_queue != last_print_queue:
-            self.send_queue()
+        if self._print_queue != last_print_queue:
+            self._send_queue_to_clients()
 
 
         return flask.make_response("POST successful", 200)
@@ -46,27 +46,27 @@ class PrintQueuePlugin(octoprint.plugin.TemplatePlugin,
     @octoprint.plugin.BlueprintPlugin.route("/start", methods=["POST"])
     @restricted_access
     def start_queue(self):
-        self.print_queue = []
+        self._print_queue = []
         for v in flask.request.form:
             j = json.loads(v)
             for p in j:
-                self.print_queue += [p]
+                self._print_queue += [p]
 
-        self.print_from_queue()
+        self._print_from_queue()
 
         return flask.make_response("POST successful", 200)
 
-    def print_from_queue(self):
-        if len(self.print_queue) > 0:
-            f = os.path.join(self.uploads_dir, self.print_queue[0])
+    def _print_from_queue(self):
+        if len(self._print_queue) > 0:
+            f = os.path.join(self._uploads_dir, self._print_queue[0])
             self._logger.info("PQ: attempting to select and print file: " + f)
             self._printer.select_file(f, False, True)
 
 
-    def send_queue(self):
+    def _send_queue_to_clients(self):
         self._plugin_manager.send_plugin_message(self._identifier, dict(
             type="set_queue",
-            print_queue=self.print_queue
+            _print_queue=self._print_queue
         ))
 
 
@@ -83,13 +83,13 @@ class PrintQueuePlugin(octoprint.plugin.TemplatePlugin,
     # AssetPlugin
     def get_assets(self):
         return dict(
-            js=["js/jquery-ui.min.js", "js/knockout-sortable.js", "js/print_queue.js"]
+            js=["js/jquery-ui.min.js", "js/knockout-sortable.js", "js/_print_queue.js"]
     )
 
 
     # Hooks
     def alter_print_completion_script(self, comm_instance, script_type, script_name, *args, **kwargs):
-        if script_type == "gcode" and script_name == "afterPrintDone" and len(self.print_queue) > 0:
+        if script_type == "gcode" and script_name == "afterPrintDone" and len(self._print_queue) > 0:
             prefix = self._settings.get(["bed_clear_script"])
             postfix = None
             return prefix, postfix
@@ -103,35 +103,35 @@ class PrintQueuePlugin(octoprint.plugin.TemplatePlugin,
     # Event Handling
     def on_event(self, event, payload):
         if event == "ClientOpened":
-            self.send_queue()
+            self._send_queue_to_clients()
 
         if event == "FileAdded":
             # TODO: add setting for auto queue
-            self.print_queue.append(payload["path"])
-            self.send_queue()
+            self._print_queue.append(payload["path"])
+            self._send_queue_to_clients()
 
         if event == "FileRemoved":
-            new_queue = [f for f in self.print_queue if f != payload["path"]]
-            if new_queue != self.print_queue:
-                self.print_queue = new_queue
-                self.send_queue()
+            new_queue = [f for f in self._print_queue if f != payload["path"]]
+            if new_queue != self._print_queue:
+                self._print_queue = new_queue
+                self._send_queue_to_clients()
 
         if event == "PrintStarted":
-            self.print_completed = False
+            self._print_completed = False
             # TODO: check if the print is in the queue
 
 
         if event == "PrintDone":
-            self.print_completed = True
+            self._print_completed = True
 
         if event == "PrinterStateChanged":
             state = self._printer.get_state_id()
             self._logger.info("printer state: " + state)
             if state  == "OPERATIONAL":
-                if self.print_completed and len(self.print_queue) > 1:
-                    self.print_queue.pop(0)
-                    self.print_from_queue()
-                    self.send_queue()
+                if self._print_completed and len(self._print_queue) > 1:
+                    self._print_queue.pop(0)
+                    self._print_from_queue()
+                    self._send_queue_to_clients()
 
 
         return
