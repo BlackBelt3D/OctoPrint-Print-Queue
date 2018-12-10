cdb6c7292ffdf927ca67cd6ea42df7709aacb6b3
fieldOfView <aldo@fieldofview.com>
2018-12-10 10:27:29 -0800

    Update author metadata
    Since this fork turned out to be a complete rewrite, bothering the original author with support requests would be wrong.

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ plugin_version = "0.2.0"
 plugin_description = """Enables continuous printing by printing an object, clearing the bed, and then printing another."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Michael New"
+plugin_author = "Aldo Hoeben"
 
 # The plugin's author's mail address.
-plugin_author_email = "mikenew12@gmail.com"
+plugin_author_email = "aldo@fieldofview.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
 plugin_url = "https://github.com/blackbelt3d/OctoPrint-Print-Queue"
